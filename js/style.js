2d65f572b9c5e952f02600074d295682a3fd01db
Agatha-girl <18779359726@qq.com>
2021-03-13 15:20:34 +0800

    上次文件

---
+++ js/style.js
@@ -0,0 +1,9 @@
+const input =document.querySelector("input");
+const label =document.querySelector("label")
+
+input.addEventListener("input",event=>{
+   const value=Number(input.value)/100;
+   input.style.setProperty('--thumb-rotate',`${value*750}deg`);
+   label.innerHTML=Math.round(value*100);
+
+})
